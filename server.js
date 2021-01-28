53d8dd96312c2c679ac7412ec71ce8d2bb812697
mohammed K <m159.l159@gmail.com>
2021-01-28 22:27:20 +0300

    finish the p3

--- server.js
+++ server.js
@@ -1,19 +1,54 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
-
-// Require Express to run server and routes
+const projectData = [];
+// Express to run server and routes
+const express = require('express');
+const app = express();
 
 // Start up an instance of app
 
+/* Dependencies */
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+// Spin up the server
+// Callback to debug
+const port = 333;
+const server = app.listen(port, checkBack);
 
-// Setup Server
+function checkBack(){
+    console.log("server running at port: " + port);
+}
+// Initialize all route with a callback function
+
+// Callback function to complete GET '/all'
+app.get('/all', (req, res) => {
+    res.json(
+        {
+            status: 'success',
+            data: projectData,
+            message: 'retrieved data',
+        }
+    )
+});
+// Post Route
+app.post('/send', (req, res) => {
+    const tmpData = {
+        temp: req.temp,
+        date: req.date,
+        userFeel: req.userFeel,
+    };
+
+    projectData.push(tmpData);
+
+    res.send(projectData);
+})
--- website/app.js
+++ website/app.js
@@ -1,5 +1,75 @@
-/* Global Variables */
+// Personal API Key for OpenWeatherMap API
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+const apiKey = '25b7a4527f77b209b126a463a9baa9c0';
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener("click", getCurrentWeather);
+
+
+/* Function called by event listener */
+function getCurrentWeather() {
+    console.log('clicked');
+    // const enteredZipCode = document.getElementById('zip').value;
+    const zipCode = document.getElementById('zip').value;
+    const apiCall = baseUrl + '?zip=' + zipCode + '&appid=' + apiKey;
+    // const apiCall = 'http://api.openweathermap.org/data/2.5/weather?zip=94040&appid=25b7a4527f77b209b126a463a9baa9c0';
+    getWeatherAPI(apiCall)
+        .then(function (d){
+            postData('/send', d)
+                .finally(updateUI)});
+}
+
+/* Function to GET Web API Data*/
+const getWeatherAPI = async (fullApiUrl) => {
+
+    const response = await fetch(fullApiUrl);
+    try {
+        return await response.json();
+    }catch (e) {
+        alert('Error: ' + e);
+    }
+}
+
+/* Function to POST data */
+const postData = async(URL = '/send', data = {}) => {
+    const userFeeling = document.getElementById('feelings').value;
+    let date = new Date();
+    let newD = date.getDate() + '/' + date.getMonth()+1 + '/' + date.getFullYear();
+
+    const payload = {
+        temp: data.main.temp,
+        date: newD,
+        userFeel: userFeeling,
+    };
+
+    const response = await fetch(URL , {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+    });
+
+    try{
+        console.log('between')
+        return await response.json();
+
+    }catch (e) {
+        alert('Error post res: ' + e)
+    }
+}
+
+/* Function to Update the UI */
+const updateUI = async () => {
+    const request = await fetch('/all');
+    try{
+        const allData = await request.json();
+        document.getElementById('date').innerHTML = allData.data.date;
+        document.getElementById('temp').innerHTML = allData.data.temp;
+        document.getElementById('content').innerHTML = allData.data.userFeel;
+    }catch(error){
+        console.log("error", error);
+    }
+}
